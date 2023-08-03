e1c926b085fbf85378b6bb0c7d71644fd4d40c27
Eduardo Ramirez <ramirezcota.eduardo@gmail.com>
2023-08-02 22:10:53 -0700

    Fix time: 1 am to be executed at 0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 console.log("[*] Started at: ", new Date().toString());
 const API_PATH = process.env.NEXTAUTH_URL || "";
 
-cron.schedule("1 23 * * *", async () => {
+cron.schedule("1 1 * * *", async () => {
   console.log("--> Calling api: ", API_PATH, "...");
   const res = await axios.get(API_PATH + "/api/rents/generateRecord");
   console.log("Finished:  ", res.data);
