39dd8d72bfd81436b347ab10d46ab987293205d7
Eduardo Ramírez <ed00001110@gmail.com>
2024-01-30 19:41:32 -0600

    Added:  Machines report generator at the end of month

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import cron from "node-cron";
-import 'dotenv/config'
+import "dotenv/config";
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
 
 console.log("[*] Started at: ", new Date().toString());
 const API_PATH = process.env.NEXTAUTH_URL || "";
@@ -9,4 +11,14 @@ cron.schedule("1 1 * * *", async () => {
   console.log("--> Calling api: ", API_PATH, "...");
   const res = await axios.get(API_PATH + "/api/rents/generateRecord");
   console.log("Finished:  ", res.data);
+});
+
+cron.schedule("58 23 * * *", async () => {
+  if (today.getMonth() !== tomorrow.getMonth()) {
+    console.log("--> Calling api for Machines Record: ", API_PATH, "...");
+    const res = await axios.get(
+      API_PATH + "/api/rents/generateRecord?id=MACHINES_REPORT"
+    );
+    console.log("Finished:  ", res.data);
+  }
 });
