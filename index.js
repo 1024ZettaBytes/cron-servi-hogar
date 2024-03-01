9be51844003a1206967fc9461de1ec0cffb16e96
Eduardo Ramírez <ed00001110@gmail.com>
2024-03-01 09:04:36 -0700

    FIX: date initi

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 import cron from "node-cron";
 import "dotenv/config";
-const today = new Date();
-const tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
 
 console.log("[*] Started at: ", new Date().toString());
 const API_PATH = process.env.NEXTAUTH_URL || "";
@@ -15,6 +12,9 @@ cron.schedule("1 1 * * *", async () => {
 });
 
 cron.schedule("58 23 * * *", async () => {
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
   if (today.getMonth() !== tomorrow.getMonth()) {
     console.log("--> Calling api for Machines Record: ", API_PATH, "...");
     const res = await axios.get(
